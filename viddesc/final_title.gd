551335c1a5f546501093b0bb51e1605110486f90
Moonlight Feather <flyingfeather1501@gmail.com>
2016-09-18 02:23:06 +0800

    Added fields etc. for the description

--- viddesc/final_title.gd
+++ viddesc/final_title.gd
@@ -10,7 +10,8 @@ var PV = ""
 var UTAU = ""
 var NAME = "name"
 var FEAT = "feat"
-var EXTRA = "extra"
+var EXTRA1 = ""
+var EXTRA2 = ""
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -18,7 +19,8 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	var extra_raw = get_node("/root/Control/Container/vbox_title/field_extra").get_text()
+	var extra1_raw = get_node("/root/Control/Container/vbox_title/field_extra_1").get_text()
+	var extra2_raw = get_node("/root/Control/Container/vbox_title/field_extra_2").get_text()
 	var feat_raw = get_node("/root/Control/Container/vbox_title/field_feat").get_text()
 	if pv_tag == true:
 		PV = "【PV】"
@@ -30,10 +32,15 @@ func _fixed_process(delta):
 	else:
 		UTAU = ""
 	
-	if extra_raw == "":
-		EXTRA = ""
+	if extra1_raw == "":
+		EXTRA1 = ""
 	else:
-		EXTRA = "【"+extra_raw+"】"
+		EXTRA1 = "【"+extra1_raw+"】"
+	
+	if extra2_raw == "":
+		EXTRA2 = ""
+	else:
+		EXTRA2 = "【"+extra2_raw+"】"
 	
 	if feat_raw == "":
 		FEAT = ""
@@ -42,7 +49,7 @@ func _fixed_process(delta):
 	
 	NAME = get_node("/root/Control/Container/vbox_title/field_name").get_text()
 	
-	self.set_text(PV+NAME+FEAT+UTAU+EXTRA)
+	self.set_text(PV+NAME+FEAT+UTAU+EXTRA1+EXTRA2)
 
 func _on_checkbox_PV_toggled( check_state ):
 	pv_tag = check_state
